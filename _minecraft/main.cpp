23c93c8049e1b32306549138f17755dd0304708e
CregskiN <272473132@qq.com>
2020-11-21 02:11:32 +0800

    Initial Commit

---
+++ _minecraft/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  _minecraft
+//
+//  Created by 吴昊 on 2020/11/21.
+//  Copyright © 2020 CregskiN. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
