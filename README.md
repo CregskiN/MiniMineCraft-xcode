1d43dd69378dee6ee904254e3b59595fe47dc11a
CregskiN <272473132@qq.com>
2020-12-13 09:04:36 +0800

    docs: ✏️ fix doc

--- README.md
+++ README.md
@@ -1,10 +1,93 @@
 # MiniMineCraft - xcode
 
-非常喜欢[wishForget](https://github.com/wishForget)做的项目**[ MiniMineCraft](https://github.com/wishForget/MiniMineCraft)**，美中不足的是只有visual studio版本，而visual studio for mac无法编写c++项目。
-
-遂将[MiniMineCraft](https://github.com/wishForget/MiniMineCraft)迁徙到xcode平台。期间踩了非常多的坑，正在询问原作者是否可以在此呈现迁徙步骤，目前本项目仅作为本人的学习记录
+本项目来源于[wishForget](https://github.com/wishForget)开发的[MiniMineCraft](https://github.com/wishForget/MiniMineCraft)，此处为Xcode版本（已获原作者授权）
 
 
 
-再次感谢[wishForget](https://github.com/wishForget)的开源共享精神！
+## 1. 使用开源库
+
+| 库名      | 功能                                     | github 链接                             |
+| --------- | ---------------------------------------- | --------------------------------------- |
+| glew      | 识别openGL版本并提供gl、glu、glext等函数 | https://github.com/nigels-com/glew      |
+| glad      | 管理OpenGL函数指针                       | https://github.com/Dav1dde/glad         |
+| glfw3     | 提供跨平台窗口                           | https://github.com/glfw/glfw            |
+| glm       | 创建矩阵向量及进行矩阵运算               | https://github.com/Groovounet/glm       |
+| assimp    | 模型加载                                 | https://github.com/assimp/assimp        |
+| stb_image | 读取纹理/贴图资源                        | https://github.com/servo/rust-stb-image |
+| Craft     | 提供方块贴图资源                         | https://github.com/fogleman/Craft       |
+| noise     | 提供噪声函数                             | https://github.com/caseman/noise        |
+
+
+
+
+
+## 2. 项目启动需要的文件
+
++ 动态库文件，见文件夹 dylibs
+
+  ```shell
+   cregskin@wuhaodeMacBook-Pro  ~/code/c/_minecraft   master ●✚  la -l ./dylibs
+  total 28328
+  -r--r--r--  1 cregskin  staff    84K 11 21 10:10 libBullet3Dynamics.2.89.dylib
+  -r--r--r--  1 cregskin  staff   102K 12 21  2019 libBullet3Dynamics.a
+  -r--r--r--  1 cregskin  staff   551K 11 21 10:10 libBulletDynamics.2.89.dylib
+  -r--r--r--  1 cregskin  staff   885K 12 21  2019 libBulletDynamics.a
+  -r--r--r--  1 cregskin  staff   448K  9 15 23:00 libGLEW.2.1.0.dylib
+  -rw-r--r--  1 cregskin  staff   105K 11 21 10:10 libLinearMath.2.89.dylib
+  -r--r--r--  1 cregskin  staff   123K 12 21  2019 libLinearMath.a
+  -r--r--r--  1 cregskin  staff    11M 11 21 02:55 libassimp.5.0.0.dylib
+  -r--r--r--  1 cregskin  staff   249K  9 15 22:38 libglfw.3.3.dylib
+  ```
+
+  
+
++ 库头文件，见文件夹 headerfiles
+
+  ```shell
+   cregskin@wuhaodeMacBook-Pro  ~/code/c/_minecraft   master ●✚  ls -la ./headerfiles
+  total 24
+  drwxr-xr-x  16 cregskin  staff    512 12 13 08:54 .
+  drwxr-xr-x  10 cregskin  staff    320 12 13 09:01 ..
+  -rw-r--r--@  1 cregskin  staff  10244 12 13 08:54 .DS_Store
+  drwxr-xr-x   6 cregskin  staff    192  7  5 10:29 Bullet3Collision
+  drwxr-xr-x  26 cregskin  staff    832  7  5 10:29 Bullet3Common
+  drwxr-xr-x   8 cregskin  staff    256  7  5 10:29 Bullet3Dynamics
+  drwxr-xr-x  10 cregskin  staff    320  7  5 10:29 Bullet3Geometry
+  drwxr-xr-x  10 cregskin  staff    320  7  5 10:29 Bullet3OpenCL
+  drwxr-xr-x   3 cregskin  staff     96  7  5 10:29 Bullet3Serialize
+  drwxr-xr-x   9 cregskin  staff    288  7  5 10:29 BulletCollision
+  drwxr-xr-x  10 cregskin  staff    320  7  5 10:29 BulletDynamics
+  drwxr-xr-x  13 cregskin  staff    416  7  5 10:29 BulletInverseDynamics
+  drwxr-xr-x  49 cregskin  staff   1568  7  5 10:29 BulletSoftBody
+  drwxr-xr-x   4 cregskin  staff    128  7  5 10:29 clew
+  drwxr-xr-x   3 cregskin  staff     96  9 15 23:44 glad
+  drwxr-xr-x  30 cregskin  staff    960 11 16 21:10 glm
+  ```
+
+
+
+
+
+## 3. 全局参数设置
+
+全局参数位于WorldInfo.h&WorldInfo.cpp中，会影响运行性能的参数如下：
+
+| 参数名            | 说明           | 备注                                                         |
+| ----------------- | -------------- | ------------------------------------------------------------ |
+| renderChunkRadius | 渲染半径       | 设置的越大可见范围越广，但可能会造成卡顿。默认为2，建议设置为1 |
+| useLight          | 是否开启光照明 | 默认开启                                                     |
+| useFog            | 是否开启雾效   | 默认开启                                                     |
+| rayTestStepNum    | 射线检测步数   | 和rayTestStep共同决定射线检测的精度和距离                    |
+| rayTestStep       | 射线检测步长   | 同rayTestStepNum                                             |
+| waterLevel        | 水平面         |                                                              |
+
+
+
+
+
+
+
+## 致谢
+
+[wishForget](https://github.com/wishForget)
 
--- _minecraft/main.cpp
+++ _minecraft/main.cpp
@@ -36,82 +36,83 @@ glm::mat4 views;
 glm::mat4 projects;
 
 void initGame() {
-	try {
-		skyDome = new SkyDome(
-			"/Users/cregskin/code/c/_minecraft/_minecraft/resources/skyDome/blueSkyWithCloud",
-			"Cam_2_Left+X.png", "Cam_3_Right-X.png",
-			"Cam_4_Up+Y.png", "Cam_5_Down-Y.png",
-			"Cam_0_Front+Z.png", "Cam_1_Back-Z.png",
-			"/Users/cregskin/code/c/_minecraft/_minecraft/shader/skyDome.vs", "/Users/cregskin/code/c/_minecraft/_minecraft/shader/skyDome.fs");
-		skyDome->init();
-		cout << "天顶球加载完毕" << endl;
-	}
-	catch (std::bad_alloc) {
-		cout << "天顶球加载出现错误" << endl;
-	}
-	camera = new Camera(glm::vec3(rand()%180, 40.0f, rand()%180));
-	Control::init(SCR_WIDTH, SCR_HEIGHT, camera, true);
-	WorldInfo::lastUpdatePostion = camera->Position;
+  try {
+    skyDome = new SkyDome(
+      "/Users/cregskin/code/c/_minecraft/_minecraft/resources/skyDome/blueSkyWithCloud",
+      "Cam_2_Left+X.png", "Cam_3_Right-X.png",
+      "Cam_4_Up+Y.png", "Cam_5_Down-Y.png",
+      "Cam_0_Front+Z.png", "Cam_1_Back-Z.png",
+      "/Users/cregskin/code/c/_minecraft/_minecraft/shader/skyDome.vs", "/Users/cregskin/code/c/_minecraft/_minecraft/shader/skyDome.fs");
+    skyDome->init();
+    cout << "天顶球加载完毕" << endl;
+  }
+  catch (std::bad_alloc) {
+    cout << "天顶球加载出现错误" << endl;
+  }
+  camera = new Camera(glm::vec3(rand()%180, 40.0f, rand()%180));
+  Control::init(SCR_WIDTH, SCR_HEIGHT, camera, true);
+  WorldInfo::lastUpdatePostion = camera->Position;
 }
 
 
 
 int main(int nargs, char *args[])
 {
-	glfwInit();
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     #ifdef __APPLE__
         glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     #endif
-	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "minecraft", NULL, NULL);
-	glfwSetFramebufferSizeCallback(window, (Control::buffer_resize_callback));
-	glfwSetCursorPosCallback(window, (Control::mouse_callback));
-	glfwSetScrollCallback(window, (Control::scroll_callback));
-	glfwSetMouseButtonCallback(window, (Control::mouse_button_callback));
+  GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "minecraft", NULL, NULL);
+  glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+  glfwSetFramebufferSizeCallback(window, (Control::buffer_resize_callback));
+  glfwSetCursorPosCallback(window, (Control::mouse_callback));
+  glfwSetScrollCallback(window, (Control::scroll_callback));
+  glfwSetMouseButtonCallback(window, (Control::mouse_button_callback));
 
-	if (window == NULL)
-	{
-		std::cout << "Failed to create GLFW window" << std::endl;
-		glfwTerminate();
-		return -1;
-	}
-	glfwMakeContextCurrent(window);
-	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-	{
-		std::cout << "Failed to initialize GLAD" << std::endl;
-		return -1;
-	}
-	glEnable(GL_DEPTH_TEST);
-	glEnable(GL_BLEND);
-	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-	glClearColor(0.6, 0.6, 0.6, 1);
-	initGame();
-	//World* world = new World();
-	//WorldInfo::world = World;
-	while (!glfwWindowShouldClose(window))
-	{
-		float currentFrame = glfwGetTime();
-		Control::deltaTime = currentFrame - Control::lastFrame;
-		Control::lastFrame = currentFrame;
-		Control::processInput(window);
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  if (window == NULL)
+  {
+    std::cout << "Failed to create GLFW window" << std::endl;
+    glfwTerminate();
+    return -1;
+  }
+  glfwMakeContextCurrent(window);
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+  {
+    std::cout << "Failed to initialize GLAD" << std::endl;
+    return -1;
+  }
+  glEnable(GL_DEPTH_TEST);
+  glEnable(GL_BLEND);
+  glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+  glClearColor(0.6, 0.6, 0.6, 1);
+  initGame();
+  //World* world = new World();
+  //WorldInfo::world = World;
+  while (!glfwWindowShouldClose(window))
+  {
+    float currentFrame = glfwGetTime();
+    Control::deltaTime = currentFrame - Control::lastFrame;
+    Control::lastFrame = currentFrame;
+    Control::processInput(window);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-		glm::mat4 projection = glm::perspective(glm::radians(camera->Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 960.0f);
-		glm::mat4 view = camera->GetViewMatrix();
-		glm::mat4 model = glm::mat4(1.0f);
+    glm::mat4 projection = glm::perspective(glm::radians(camera->Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 960.0f);
+    glm::mat4 view = camera->GetViewMatrix();
+    glm::mat4 model = glm::mat4(1.0f);
 
-		WorldInfo::projection = projection;
-		WorldInfo::view = view;
-		skyDome->StateChange(glm::vec3(0, 0, 0), glm::vec3(200, 200, 200), projection, view);
-		skyDome->Render();
-		//world->render();
-		World::render();
-		glfwSwapBuffers(window);
-		glfwPollEvents();
-	}
+    WorldInfo::projection = projection;
+    WorldInfo::view = view;
+    skyDome->StateChange(glm::vec3(0, 0, 0), glm::vec3(200, 200, 200), projection, view);
+    skyDome->Render();
+    //world->render();
+    World::render();
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
 
-	glfwTerminate();
-	return 0;
+  glfwTerminate();
+  return 0;
 }
